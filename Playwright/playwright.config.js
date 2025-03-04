66f697fd4c466baab1d29ded75b55b3c5725896e
1512toannguyenmoso <toan.nguyen@moso.com>
2025-03-04 13:48:24 +0700

    practice on March 4th,2025

--- Playwright/playwright.config.js
+++ Playwright/playwright.config.js
@@ -36,11 +36,11 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  // projects: [
-  //   {
-  //     name: 'chromium',
-  //     use: { ...devices['Desktop Chrome'] },
-  //   },
+  projects: [
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
 
     // {
     //   name: 'firefox',
