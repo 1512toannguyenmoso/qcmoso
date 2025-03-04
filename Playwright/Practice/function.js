e0c3dd76422006f3314b8a2f7e2c8b72a6993f1a
1512toannguyenmoso <toan.nguyen@moso.com>
2025-03-04 13:25:43 +0700

    practice March 4th, 2025

--- Playwright/Practice/function.js
+++ Playwright/Practice/function.js
@@ -12,4 +12,19 @@ async function loginFailed(page, url, email, password, errorMessage) {
     await page.click('//button[@id="gwt-debug-submit"]');
     await expect(page.locator('//p[@id="error"]')).toHaveText(errorMessage);
 };
-module.exports = {loginFunction, loginFailed};
+
+function generateRandomEmail(){
+    const char = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const domainNames = ["test.com","testing.com","abctest.com","xyztest.com","noexistingtest.com"];
+    function getRandomString(length){
+        let result = "";
+        for (let i=0;i < length; i++){
+            result += char.charAt(Math.floor(Math.random()*char.length));
+        }
+        return result; 
+    }
+    const username = getRandomString(8);
+    const domain = domainNames[Math.floor(Math.random() * domainNames.length)];
+    return `${username}@${domain}`;
+};
+module.exports = {loginFunction, loginFailed, generateRandomEmail};
--- Playwright/Practice/practice.spec.js
+++ Playwright/Practice/practice.spec.js
@@ -1,23 +1,17 @@
 const{test, expect} = require('@playwright/test');
 //import {test, expect} from '@playwright/test';
+const myFunctions = require('./function')
 
 test.beforeAll('Navigate to the landing page', async({page})=>{
   await page.goto('https://www.viet18.com');
 });
-//Login page
-test.describe('User Authentication Tests',() =>{
+//User authentication, User is admin.
+test.describe('User Authentication Tests, User is admin',() =>{
   test('Login Home Page Successfully',async ({page})=>{
-    await page.goto('https://www.viet18.com/login');
-    await page.fill('//input[@id="email"]','chauchau.inc@gmail.com');
-    await page.fill('//input[@id="password"]','Phuong123456');
-    await page.click('//button[@id="gwt-debug-submit"]');
-    });
+    await myFunctions.loginFunction(page, 'https://www.viet18.com/login', 'chauchau.inc@gmail.com', '123456');
+  });
   test('Login Home Page Failed', async({page})=>{
-    await page.goto('https://www.viet18.com/login');
-    await page.fill('//input[@id="email"]','chauchau.inc@gmail.com');
-    await page.fill('//input[@id="password"]','Phuong12345');
-    await page.click('//button[@id="gwt-debug-submit"]');
-    await expect(page.locator('//p[@id="error"]')).toHaveText("Login failed. Invalid email or password.");
+    await myFunctions.loginFailed(page, 'https://www.viet18.com/login', 'chauchau.inc@gmail.com', '123455', 'Login failed. Invalid email or password.');
   });
   test('Test reset password of non-existing account', async({page})=>{
     await page.goto('https://www.viet18.com/reset_password');
@@ -26,3 +20,41 @@ test.describe('User Authentication Tests',() =>{
     await expect(page.locator('//p[@id="error"]')).toHaveText("There is no account associated set this email address.");
 })
 });
+//user authentication, user is borrower.
+test.describe('User Authentication Tests, User is borrower',() => {
+  test('Register user', async({page})=>{
+    await page.goto('https://www.viet18.com/apply');
+    await page.click("//p[normalize-space()='I want to BUY A HOME']");
+    await page.click("//p[normalize-space()='Seller accepted my offer']");
+    await page.fill("//input[@id='email']",myFunctions.generateRandomEmail());
+    await page.click("//button[@id='gwt-debug-next']");
+    await page.fill("//input[@id='password']",'123456');
+    await page.click("//button[3]");
+  })
+  test('Login Home Page Successfully',async ({page})=>{
+    await myFunctions.loginFunction(page, 'https://www.viet18.com/login', 'userisborrower@testing.com', '123456');
+  });
+  test('Login Home Page Failed', async ({page}) => {
+    await myFunctions.loginFailed(page, 'https://www.viet18.com/login', 'userisborrower@testing.com', '123455', "Login failed. Invalid email or password.");
+  });
+});
+//user authentication, user is lender user.
+test.describe('User Authentication Tests, User is lender user',()=>{
+  test('Login Home Page Successfully',async ({page})=>{
+    await myFunctions.loginFunction(page, 'https://www.viet18.com/login', 'userislenderuser@test.com', '123456');
+  });
+  test('Login Home Page Failed', async ({page}) => {
+    await myFunctions.loginFailed(page, 'https://www.viet18.com/login', 'userislenderuser@test.com', '123455', "Login failed. Invalid email or password.");
+  });
+});
+//user authentication, user is vendor user.
+test.describe('User Authentication Tests, User is vendor user',()=>{
+  test('Login Home Page Successfully',async ({page})=>{
+    await myFunctions.loginFunction(page, 'https://www.viet18.com/login', 'userisvendor@test.com', '123456');
+  });
+  test('Login Home Page Failed', async ({page}) => {
+    await myFunctions.loginFailed(page, 'https://www.viet18.com/login', 'userisvendor@test.com', '123455', "Login failed. Invalid email or password.");
+  });
+});
+
+
