3f3e8b9904dec7d4db7efd3266260c390f594a70
Tran Quang Hien <tranquanghienict@gmail.com>
2025-03-06 09:26:30 +0700

    Practice on 05/03/2025

--- Playwright/js/advance-loops.js
+++ Playwright/js/advance-loops.js
@@ -1,33 +1,40 @@
-let arrNumbers = [3, 5, 6, 2, 1, 7, 8, 97, 11];
+// let arrNumbers = [3, 5, 6, 2, 1, 7, 8, 97, 11];
 
-function isPrime(num) {
-  if (num < 2) {
-    return false;
-  } else {
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i == 0) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
+// function isPrime(num) {
+//   if (num < 2) {
+//     return false;
+//   } else {
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//       if (num % i == 0) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// }
 
-// Using for..of loop
+// // Using for..of loop
 
-for (let temp of arrNumbers) {
-  console.log(`Is ${temp} a prime number? ${isPrime(temp)}`);
-}
+// for (let temp of arrNumbers) {
+//   console.log(`Is ${temp} a prime number? ${isPrime(temp)}`);
+// }
 
 // Using forEach loop
-
-arrNumbers.forEach((value, index) => {
-  console.log(`Is ${value} a prime number? ${isPrime(value)}`);
-  console.log(`And has index = ${index}`);
-});
-
-// Using for..in loop
-
-for (let i in arrNumbers) {
-  console.log(arrNumbers[i]);
+// Arrow function
+// Call back
+function callAFunction(isPrime) {
+  return isPrime(2);
 }
+
+console.log(callAFunction(isPrime));
+
+// arrNumbers.forEach((value, index) => {
+//   console.log(`Is ${value} a prime number? ${isPrime(value)}`);
+//   console.log(`And has index = ${index}`);
+// });
+
+// // Using for..in loop
+// // for..in for Object
+// for (let i in arrNumbers) {
+//   console.log(arrNumbers[i]);
+// }
