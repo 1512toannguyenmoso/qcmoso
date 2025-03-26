24d61bc3861e92bd83de9b1d6db95944054219aa
1512toannguyenmoso <toan.nguyen@moso.com>
2025-03-26 17:21:08 +0700

    save a toan life

--- Playwright/Practice/BasePage.js
+++ Playwright/Practice/BasePage.js
@@ -1,5 +1,7 @@
 const {expect} = require('@playwright/test');
 const {chromium} = require('playwright');
+import helpers_function from '../utils/helpers.js';
+import datatest from '../utils/datatest.js';
 
 
 export class LoginPage{
@@ -21,6 +23,7 @@ export class LoginPage{
     await this.usernameInput_locator.fill(username);
     await this.passwordInput_locator.fill(password);
     await this.loginButton_locator.click();
+    await this.page.waitForLoadState('domcontentloaded'); 
   }
   async resetPassword(email){
     await this.page.signInbutton_locator.click();
@@ -48,20 +51,14 @@ export class LoginPage{
 export class ApplicationPage{
   constructor(page){
     this.page = page;
-    this.loanApplication_locator = this.page.locator("//a[normalize-space()='Loan Application']");
-    this.buyAHome_Locator = this.page.locator('button:has-text("I want to BUY A HOME")');
-    this.preapprovalLetter_Locator = this.page.locator('button:has-text("I want a pre-approval letter")');
-    this.sellerAcceptedMyOffer = this.page.locator('button:has-text("Seller accepted my offer")');
-    this.refinance_locator = this.page.locator('button:has-text("I want to REFINANCE")');
-    this.refinanceRatePayment_Locator = this.page.locator('button:has-text("I want lower rates/payments")');
-    this.refinanceCashOut_Locator = this.page.locator('button:has-text("I want cash out")');
+    this.loanApplication_Locator = this.page.locator("//a[normalize-space()='Loan Application']");
     this.userNameRegister_Locator = this.page.locator("//input[@id='email']");
     this.passwordRegister_Locator = this.page.locator("//input[@id='password']");
     this.createNewAccount_Locator = this.page.locator("//button[3]");
     this.nextButton_Locator = this.page.locator("//button[@id='gwt-debug-next']");
   };
   async CreatePreApprovalLetter(username,password){
-    await this.loanApplication_locator.click();
+    await this.loanApplication_Locator.click();
     await this.page.waitForLoadState('networkidle');
     await this.buyAHome_Locator.click();
     await this.page.waitForLoadState('networkidle');
@@ -72,7 +69,7 @@ export class ApplicationPage{
     await this.createNewAccount_Locator.click();
   };
   async CreatePurchaseLoan(username,password){
-    await this.loanApplication_locator.click();
+    await this.loanApplication_Locator.click();
     await this.page.waitForLoadState('networkidle');
     await this.buyAHome_Locator.click();
     await this.page.waitForLoadState('networkidle');
@@ -83,9 +80,9 @@ export class ApplicationPage{
     await this.createNewAccount_Locator.click();
   };
   async CreateRefinanceRateTermLoan(username,password){
-    await this.loanApplication_locator.click();
+    await this.loanApplication_Locator.click();
     await this.page.waitForLoadState('networkidle');
-    await this.refinance_locator.click();
+    await this.refinance_Locator.click();
     await this.page.waitForLoadState('networkidle');
     await this.refinanceRatePayment_Locator.click();
     await this.userNameRegister_Locator.fill(username);
@@ -94,9 +91,9 @@ export class ApplicationPage{
     await this.createNewAccount_Locator.click();
   };
   async CreateRefinanceCashOutLoan(username,password){
-    await this.loanApplication_locator.click();
+    await this.loanApplication_Locator.click();
     await this.page.waitForLoadState('networkidle');
-    await this.refinance_locator.click();
+    await this.refinance_Locator.click();
     await this.page.waitForLoadState('networkidle');
     await this.refinanceCashOut_Locator.click();
     await this.userNameRegister_Locator.fill(username);
@@ -105,3 +102,79 @@ export class ApplicationPage{
     await this.createNewAccount_Locator.click();
   };
 };
+export class BorrowerPortal{
+  constructor(page){
+    this.page = page;
+    this.startApplication_Locator = this.page.locator("//i[normalize-space()='work']");
+    this.activeApplication_Locator = this.page.locator("//span[normalize-space()='Active application']");
+    this.todo_Locator = this.page.locator("//span[normalize-space()='To-dos']");
+    this.quote_Locator = this.page.locator("//span[normalize-space()='Quote']");
+    this.allTransactions_Locator = this.page.locator("//span[normalize-space()='All transactions']");
+    this.myProfile_Locator = this.page.locator("//span[@class='d-none d-md-inline']");
+    this.notification_Locator = this.page.locator("//i[normalize-space()='notifications']");
+  };
+};
+export class ApplicationForm1003{
+  constructor(page){
+    this.page = page;
+    this.helpers_function = helpers_function;
+    //Borrower portal, Start new application tab
+    this.startNewApplication_Locator = this.page.locator("//i[normalize-space()='work']");
+    this.buyAHome_Locator = this.page.locator('button:has-text("I want to BUY A HOME")');
+    this.preapprovalLetter_Locator = this.page.locator('button:has-text("I want a pre-approval letter")');
+    this.sellerAcceptedMyOffer = this.page.locator('button:has-text("Seller accepted my offer")');
+    this.refinance_Locator = this.page.locator('button:has-text("I want to REFINANCE")');
+    this.refinanceRatePayment_Locator = this.page.locator('button:has-text("I want lower rates/payments")');
+    this.refinanceCashOut_Locator = this.page.locator('button:has-text("I want cash out")');
+    //AI Application
+    this.SkipAndFillApplicationManually_Locator = this.page.locator("//button[@id='skip-and-fill-application-manually']");
+    this.AITermofServiceCheckBox = ['input[name="agree"]'];
+    //Generral tab
+    this.changeLenderButton_Locator = this.page.locator("//button[@id='change-lender-button']");
+    this.correspondentLenderButton_Locator = this.page.locator("//button[normalize-space()='Correspondent']");
+    this.wholesaleLenderButton_Locator = this.page.locator("//button[normalize-space()='Wholesale']");
+    this.lenderNameButton_Locator = this.page.locator("//span[@id='select2-lender-container']");
+    this.reasonsForChangeLender_Locator = this.page.locator("//textarea[@id='reason']");
+    this.ticketOwner_Locator = this.page.locator("//span[@id='select2-owner-container']");
+    this.upLoadUploadedTicketDocs_Locator = this.page.locator("//div[@class='GEVLLT2CMT progress form-control']");
+    this.submitButton_Locator = this.page.locator("//div[@class='modal-footer cleafix flex-wrap']//button[@id='gwt-debug-submit']");
+    this.changeLenderFee_Locator = this.page.locator("//input[@id='change_lender_fee']");
+    this.lenderPaidCompensation_Locator = this.page.locator("//button[normalize-space()='Lender paid']");
+    this.borrowerPaidCompensation_Locator = this.page.locator("//button[normalize-space()='Borrower paid']");
+    this.lenderPaidPercentage_Locator = this.page.locator("//input[@id='lender_paid_percentage']");
+    this.borrowerPaidPercentage_Locator = this.page.locator("//input[@id='borrower_paid_percentage']");
+    this.borrowerPaidPercentageInputAmount_Locator = this.page.locator("//button[@id='i-classmaterial-icons-unset-iconsediti-amount']");
+    this.creditReportFee_Locator = this.page.locator("//input[@id='credit_report_fee']");
+    this.processingFeeChargedToBorrower_Locator = this.page.locator("//input[@id='processing_fee_paid_by_borrower']");
+    this.commissionNotes_Locator = this.page.locator("//input[@id='commission_notes']");
+    this.loanNumber_Locator = this.page.locator("//input[@id='loan_number']");
+    this.lenderURL_Locator = this.page.locator("//input[@id='lender_url']");
+    this.loanStory_Locator = this.page.locator("//textarea[@id='lender_reason']");
+    this.loanSource_Locator = this.page.locator("//span[@id='select2-loan_source-container']");
+    this.branchName_Locator = this.page.locator("//span[@id='select2-branch-container']");
+    this.mainLoanOfficer_Locator = this.page.locator("//span[@class='twitter-typeahead']//input[@id='agent']");
+    this.additionalLoanOfficer_Locator = this.page.locator("//span[@class='twitter-typeahead']//input[@id='agent']");
+    this.sharedCommissionSplit_Locator = this.page.locator("//input[@id='shared_commission_split']");
+    this.flatAmount_Locator = this.page.locator("//input[@id='flat_amount']");
+    this.referringAssociate_Locator = this.page.locator("//input[@id='referring_associate']");
+    this.loanOfficerAssistant_Locator = this.page.locator("//input[@id='originator_assistant']");
+    this.useLoanCoordinatorservice_Locator = this.page.locator("//div[@class='tt-dataset tt-dataset-6']//div[@class='tt-suggestion tt-selectable']");
+    this.loanCoordinator_Locator = this.page.locator("//input[@id='setup_specialist']");
+    this.disclosureSpecialist_Locator = this.page.locator("//div[@id='setup_specialist']//div[@class='tt-menu']");
+    this.useAssemblyLineProcessing_Locator = this.page.locator("//div[@id='use_assembly_line']//button[@name='Yes'][normalize-space()='Yes']");;
+    this.notUseAssemblyLineProcessing_Locator = this.page.locator("//div[@id='use_assembly_line']//button[@name='No'][normalize-space()='No']");
+    this.loanProcessingTeam_Locator = this.page.locator("//span[@id='select2-processing_team-container']");
+    this.loanProcessor_Locator = this.page.locator("//span[@id='select2-processing_team-container']");
+    this.underwriter_Locator = this.page.locator("//input[@id='underwriter']");
+    this.realtorSpecialist_Locator = this.page.locator("//input[@id='realtor_specialist']");
+    this.priority_Locator = this.page.locator("//span[@class='select2 select2-container select2-container--default select2-container--focus']//span[@id='select2-priority-container']");
+    this.howDidYouHearAboutUs_Locator = this.page.locator("//span[@id='select2-referrer_linked_media-container']");
+    this.preferredLanguage_Locator = this.page.locator("//span[@class='select2 select2-container select2-container--default select2-container--focus']//input[@role='textbox']");
+    this.loanAdditionalAdjustment_Locator = this.page.locator("//input[@id='loan_additional_adjustment']");
+    this.discountCode_Locator = this.page.locator("//input[@id='code']");
+    //Loan Infor tab
+    this.nextButton_Locator = this.page.locator("//button[@id='next']");
+    this.saveAndNextButton_Locator = this.page.locator("//button[@id='gwt-debug-next']");
+    this.abc_Locator = this.page.locator("//input[@id='abc']");
+  }
+}
--- Playwright/models/UserManagement.js
+++ Playwright/models/UserManagement.js
@@ -1,9 +1,10 @@
-import {LoginPage,ApplicationPage} from '../Practice/BasePage.js';
+import {LoginPage,ApplicationPage,BorrowerPortal,ApplicationForm1003} from '../Practice/BasePage.js';
 class BaseUser{
     constructor(page){
         this.page = page;
         this.signinPage = new LoginPage(page);
-        this.ApplicationPage = new ApplicationPage(page);
+        this.ApplicationTab = new ApplicationPage(page);
+        this.ApplicationForm = new ApplicationForm1003(page)
     }
 };
 export class Borrower extends BaseUser{
@@ -13,26 +14,88 @@ export class Borrower extends BaseUser{
     async BorrowerLogin(username,password){
         await this.signinPage.login(username,password);
     };
-    async BorrowerCreatePreApprovalLoan(username,password){
-        await this.ApplicationPage.CreatePreApprovalLetter(username,password);
-    };
+    //Borrower login to borrower portal and create loan.
     async BorrowerCreatePurchaseLoan(username,password){
-        await this.ApplicationPage.CreatePurchaseLoan(username, password);
+        await this.BorrowerLogin(username,password);
+        await this.ApplicationForm.startNewApplication_Locator.click();
+        await this.ApplicationForm.buyAHome_Locator.waitFor({ state: 'visible' });
+        await this.ApplicationForm.buyAHome_Locator.click();
+        await this.ApplicationForm.sellerAcceptedMyOffer.click();
+        await this.ApplicationForm.SkipAndFillApplicationManually_Locator.click();
+    };
+    async BorrowerCreatePreApprovalLoan(username,password){
+        await this.BorrowerLogin(username,password);
+        await this.ApplicationForm.startNewApplication_Locator.click();
+        await this.ApplicationForm.buyAHome_Locator.waitFor({ state: 'visible' });
+        await this.ApplicationForm.buyAHome_Locator.click();
+        await this.ApplicationForm.preapprovalLetter_Locator.click();
+        await this.ApplicationForm.SkipAndFillApplicationManually_Locator.click();
+        
     };
     async BorrowerCreateRefinanceRateTermLoan(username,password){
-        await this.ApplicationPage.CreateRefinanceRateTermLoan(username,password);
+        await this.BorrowerLogin(username,password);
+        await this.ApplicationForm.startNewApplication_Locator.click();
+        await this.ApplicationForm.refinance_Locator.waitFor({ state: 'visible' });
+        await this.ApplicationForm.refinance_Locator.click();
+        await this.ApplicationForm.refinanceRatePayment_Locator.click();
+        await this.ApplicationForm.SkipAndFillApplicationManually_Locator.click();
     };
     async BorrowerCreateRefinanceCashOutLoan(username,password){
-        await this.ApplicationPage.CreateRefinanceCashOutLoan(username,password);
-    }
+        await this.BorrowerLogin(username,password);
+        await this.ApplicationForm.startNewApplication_Locator.click();
+        await this.page.waitForLoadState('networkidle');
+        await this.ApplicationForm.refinance_Locator.waitFor({ state: 'visible' });
+        await this.ApplicationForm.refinance_Locator.click();
+        await this.ApplicationForm.refinanceCashOut_Locator.click();
+        await this.ApplicationForm.SkipAndFillApplicationManually_Locator.click();
+    };
+    //Non logged in borrower create loan
+    async NonLoggedInBorrowerCreatePurchaseLoan(username,password){
+        await this.ApplicationTab.loanApplication_Locator.click();
+        await this.ApplicationForm.buyAHome_Locator.click();
+        await this.ApplicationForm.sellerAcceptedMyOffer.click();
+        await this.ApplicationTab.userNameRegister_Locator.fill(username);
+        await this.ApplicationTab.nextButton_Locator.click();
+        await this.ApplicationTab.passwordRegister_Locator.fill(password);
+        await this.ApplicationTab.createNewAccount_Locator.click();
+        await this.ApplicationForm.SkipAndFillApplicationManually_Locator.click();
+    };
+    async NonLoggedInBorrowerCreatePreApprovalLoan(username,password){
+        await this.ApplicationTab.loanApplication_Locator.click();
+        await this.ApplicationForm.buyAHome_Locator.click();
+        await this.ApplicationForm.preapprovalLetter_Locator.click();
+        await this.ApplicationTab.userNameRegister_Locator.fill(username);
+        await this.ApplicationTab.nextButton_Locator.click();
+        await this.ApplicationTab.passwordRegister_Locator.fill(password);
+        await this.ApplicationTab.createNewAccount_Locator.click();
+        await this.ApplicationForm.SkipAndFillApplicationManually_Locator.click();
+    };
+    async NonLoggedInBorrowerCreateRefinanceRateTermLoan(username,password){
+        await this.ApplicationTab.loanApplication_Locator.click();
+        await this.ApplicationForm.refinance_Locator.click();
+        await this.ApplicationForm.refinanceRatePayment_Locator.click();
+        await this.ApplicationTab.userNameRegister_Locator.fill(username);
+        await this.ApplicationTab.nextButton_Locator.click();
+        await this.ApplicationTab.passwordRegister_Locator.fill(password);
+        await this.ApplicationTab.createNewAccount_Locator.click();
+        await this.ApplicationForm.SkipAndFillApplicationManually_Locator.click();
+    };
+    async NonLoggedInBorrowerCreateRefinanceCashOutLoan(username,password){
+        await this.ApplicationTab.loanApplication_Locator.click();
+        await this.ApplicationForm.refinance_Locator.click();
+        await this.ApplicationForm.refinanceCashOut_Locator.click();
+        await this.ApplicationTab.userNameRegister_Locator.fill(username);
+        await this.ApplicationTab.nextButton_Locator.click();
+        await this.ApplicationTab.passwordRegister_Locator.fill(password);
+        await this.ApplicationTab.createNewAccount_Locator.click();
+        await this.ApplicationForm.SkipAndFillApplicationManually_Locator.click();
+    };
 };
 export class Admin extends BaseUser{
     constructor(page){
         super(page);
-        this.signinPage = new LoginPage(page);
     }
-    async AdminLogin(username, password){
+    async AdminLogin(username,password){
         await this.signinPage.login(username,password);
     }
-}
-
+};
