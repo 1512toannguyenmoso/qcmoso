dc2567835f403129a237e45c441db2251ed374dc
haimoso <hai@moso.com>
2025-07-18 12:29:55 +0700

    Practice 18/07

--- tests/getQuote.spec.js
+++ tests/getQuote.spec.js
@@ -1,6 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('Get Quote widget with QM Pricing', async ({ page }) => {
-    
-    
+test('Get Quote widget with QM Pricing - Comapny Website', async ({ page }) => {
+    await page.goto("https://www.viet18.com/");
+    await page.waitForLoadState('load');
+    await page.waitForTimeout(8000);
+    await page.click("//a[normalize-space()='FULL DOC']");
+    await page.click("(//button[normalize-space()='Get Quote'])[1]");
+    await page.click("span#select2-income_to_ami-container");
+    await page.keyboard.press("Enter");
+    await page.click("//div[@class='modal-footer cleafix flex-wrap']//button[@id='gwt-debug-submit']");
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: 'load' }),
+      ]);
+    await expect(page.locator("//div[contains(@class,'clearfix')]//div//div[contains(@class,'active')]")).not.toHaveText("");
 });
