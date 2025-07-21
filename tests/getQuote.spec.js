06ec9c1606c235ec8bf5e0771e7bbb7c5c9e147b
haimoso <hai@moso.com>
2025-07-21 17:39:51 +0700

    Practice 21/07

--- tests/getQuote.spec.js
+++ tests/getQuote.spec.js
@@ -2,27 +2,34 @@ import { test, expect } from '@playwright/test';
 
 test.describe.serial("List test cases get quote - Non-login", () => {
 
-  test("Get Quote widget with QM Pricing - Comapny website", async ({ page }) => {
-    await page.goto("https://www.viet18.com/");
-    await page.waitForLoadState("load");
-    await page.waitForTimeout(5000);
-    await page.click("//a[normalize-space()='FULL DOC']");
-    await page.click("(//button[normalize-space()='Get Quote'])[1]");
-    await page.click("span#select2-income_to_ami-container");
-    await page.keyboard.press("Enter");
-    await Promise.all([
-      page.waitForNavigation({ waitUntil: "load" }),
-      page.click("//div[@class='modal-footer cleafix flex-wrap']//button[@id='gwt-debug-submit']"),
-    ]);
-    await page.waitForTimeout(8000);
-    await expect(page.locator("//input[@aria-label='Row 1 Compare']")).toBeVisible();
-  });
+  // test("Get Quote widget with QM Pricing - Comapny website", async ({ page }) => {
+  //   await page.goto("https://www.viet18.com/");
+  //   await page.waitForLoadState("load");
+  //   await page.waitForTimeout(10000);
+  //   await page.click("//a[normalize-space()='FULL DOC']");
+  //   await page.click("(//button[normalize-space()='Get Quote'])[1]");
+  //   await page.click("span#select2-income_to_ami-container");
+  //   await page.keyboard.press("Enter");
+  //   await Promise.all([
+  //     page.waitForNavigation({ waitUntil: "load" }),
+  //     page.click("//div[@class='modal-footer cleafix flex-wrap']//button[@id='gwt-debug-submit']"),
+  //   ]);
+  //   await page.waitForTimeout(10000);
+  //   await expect(page.locator("//input[@aria-label='Row 1 Compare']")).toBeVisible();
+  // });
 
   test("Get quote widget with Non-QM Pricing - Company website", async ({ page }) => {
     await page.goto("https://www.viet18.com/");
     await page.waitForLoadState("load");
-    await page.waitForTimeout(5000);
-    
+    await page.waitForTimeout(7000);
+    await page.click("//div[@id='com.mvu.loan.client.quote.SmallQuoteTabPanel']//a[normalize-space()='NO-INCOME DOC']");
+    await page.click("//div[@id='com.mvu.loan.client.quote.SmallNonQmQuoteForm']//button[@id='gwt-debug-submit']");
+    await page.waitForNavigation({ waitUntil: "networkidle", timeout: 3000});
+    await page.waitForURL("**/quote/non_qm**", { timeout: 10000 });
+
+    // await page.waitForLoadState("load");
+    // await page.waitForTimeout(10000);
+    // await expect(page.locator("//input[@aria-label='Row 1 Compare']")).toBeVisible();
   });
 
 });
